0f49d64b99c87f5a076c1d1d1d6274ae38a1fc3c
Murasaki-Vien <neil.melvin.diaz@gmail.com>
2022-10-12 14:35:20 +0800

    eight commit

--- App/dashboard/route.py
+++ App/dashboard/route.py
@@ -27,28 +27,36 @@ def dashboardPage(MyAcc):
             cebu_places=CebuPlaces.query.filter_by(name=place).first()
             return redirect(url_for("dashboard.TspotsPage", MyAcc=MyAcc, spot=cebu_places.name))
         
-        elif place == "KAWASAN FALLS - BADIAN, CEBU":
+        elif place == "Kawasan Falls, Badian":
+            cebu_places=CebuPlaces.query.filter_by(name=place).first()
+            return redirect(url_for("dashboard.TspotsPage", MyAcc=MyAcc, spot=cebu_places.name))
+        
+        elif place == "LexMin Resort, Argao":
             cebu_places=CebuPlaces.query.filter_by(name=place).first()
             return redirect(url_for("dashboard.TspotsPage", MyAcc=MyAcc, spot=cebu_places.name))
 
     #for Restautrants
-        elif place == "":
+        elif place == "Anzani New Mediterranean":
             cebu_places=CebuPlaces.query.filter_by(name=place).first()
             return redirect(url_for("dashboard.TspotsPage", MyAcc=MyAcc, spot=cebu_places.name))
         
-        elif place == "":
+        elif place == "Amihan Restaurant Malapascua":
             cebu_places=CebuPlaces.query.filter_by(name=place).first()
             return redirect(url_for("dashboard.TspotsPage", MyAcc=MyAcc, spot=cebu_places.name))
         
-        elif place == "":
+        elif place == "Smooth Cafe, Moalboal":
             cebu_places=CebuPlaces.query.filter_by(name=place).first()
             return redirect(url_for("dashboard.TspotsPage", MyAcc=MyAcc, spot=cebu_places.name))
         
-        elif place == "":
+        elif place == "The Pig & Palm":
             cebu_places=CebuPlaces.query.filter_by(name=place).first()
             return redirect(url_for("dashboard.TspotsPage", MyAcc=MyAcc, spot=cebu_places.name))
         
-        elif place == "":
+        elif place == "Thai-Phoon, Bantayan Island":
+            cebu_places=CebuPlaces.query.filter_by(name=place).first()
+            return redirect(url_for("dashboard.TspotsPage", MyAcc=MyAcc, spot=cebu_places.name))
+       
+        elif place == "Entoy's Bakasihan, Cordova":
             cebu_places=CebuPlaces.query.filter_by(name=place).first()
             return redirect(url_for("dashboard.TspotsPage", MyAcc=MyAcc, spot=cebu_places.name))
 
@@ -62,78 +70,144 @@ def TspotsPage(MyAcc, spot):
     picture1 = ""
     picture2 = ""
     picture3 = ""
+    picture4 = ""
+    picture5 = ""
+    picture6 = ""
     content = ""
+    TspotsName = ""
 
     if spot == "Moalboal":
         cebu_places=CebuPlaces.query.filter_by(name=spot).first()
+        TspotsName = cebu_places.name
         content = cebu_places.information
         picture1 = "/static/tspic/Moalboal1.jpg"
         picture2 = "/static/tspic/Moalboal2.jpg"
         picture3 = "/static/tspic/Moalboal3.jpg"
+        picture4 = "/static/tspic/Moalboal4.jpg"
+        picture5 = "/static/tspic/Moalboal5.jpg"
+        picture6 = "/static/tspic/Moalboal6.jpg"
     
     elif spot == "Bantayan Island":
         cebu_places=CebuPlaces.query.filter_by(name=spot).first()
+        TspotsName = cebu_places.name
         content = cebu_places.information
         picture1 = "/static/tspic/BantayanIsland1.jpg"
         picture2 = "/static/tspic/BantayanIsland2.jpg"
         picture3 = "/static/tspic/BantayanIsland3.jpg"
+        picture4 = "/static/tspic/BantayanIsland4.jpg"
+        picture5 = "/static/tspic/BantayanIsland5.jpg"
+        picture6 = "/static/tspic/BantayanIsland6.jpg"
     
     elif spot == "Santiago White Beach":
         cebu_places=CebuPlaces.query.filter_by(name=spot).first()
         content = cebu_places.information
+        TspotsName = cebu_places.name
         picture1 = "/static/tspic/SantiagoWhiteBeach1.jpg"
         picture2 = "/static/tspic/SantiagoWhiteBeach2.jpg"
         picture3 = "/static/tspic/SantiagoWhiteBeach3.jpg"
+        picture4 = "/static/tspic/SantiagoWhiteBeach4.jpg"
+        picture5 = "/static/tspic/SantiagoWhiteBeach5.jpg"
+        picture6 = "/static/tspic/SantiagoWhiteBeach6.jpg"
 
     elif spot == "Blue Water Marigo Beach Resort":
         cebu_places=CebuPlaces.query.filter_by(name=spot).first()
         content = cebu_places.information
+        TspotsName = cebu_places.name
         picture1 = "/static/tspic/MaribagoBeachResost1.jpg"
         picture2 = "/static/tspic/MaribagoBeachResost2.jpg"
         picture3 = "/static/tspic/MaribagoBeachResost3.jpg"
+        picture4 = "/static/tspic/MaribagoBeachResost4.jpg"
+        picture5 = "/static/tspic/MaribagoBeachResost5.jpg"
+        picture6 = "/static/tspic/MaribagoBeachResost6.jpg"
 
-    elif spot == "KAWASAN FALLS - BADIAN, CEBU":
+    elif spot == "Kawasan Falls, Badian":
         cebu_places=CebuPlaces.query.filter_by(name=spot).first()
         content = cebu_places.information
+        TspotsName = cebu_places.name
         picture1 = "/static/tspic/kawasanfalls1.jpg"
         picture2 = "/static/tspic/kawasanfalls2.jpg"
-        picture3 = "/static/tspic/kawasanfalls3.jpg" 
+        picture3 = "/static/tspic/kawasanfalls3.jpg"
+        picture4 = "/static/tspic/kawasanfalls4.jpg"
+        picture5 = "/static/tspic/kawasanfalls5.jpg"
+        picture6 = "/static/tspic/kawasanfalls6.jpg"
+
+    elif spot == "LexMin Resort, Argao":
+        cebu_places=CebuPlaces.query.filter_by(name=spot).first()
+        content = cebu_places.information
+        TspotsName = cebu_places.name
+        picture1 = "/static/tspic/Lexmin1.jpg"
+        picture2 = "/static/tspic/Lexmin2.jpg"
+        picture3 = "/static/tspic/Lexmin3.jpg"
+        picture4 = "/static/tspic/lexmin4.jpg"  
+        picture5 = "/static/tspic/lexmin5.jpg"  
+        picture6 = "/static/tspic/lexmin6.jpg"  
 
     #For Restaurants
 
-    elif spot == "":
+    elif spot == "Anzani New Mediterranean":
         cebu_places=CebuPlaces.query.filter_by(name=spot).first()
+        TspotsName = cebu_places.name
         content = cebu_places.information
         picture1 = ""
         picture2 = ""
         picture3 = ""
+        picture4 = ""
+        picture5 = ""
+        picture6 = ""
     
-    elif spot == "":
+    elif spot == "Amihan Restaurant Malapascua":
         cebu_places=CebuPlaces.query.filter_by(name=spot).first()
+        TspotsName = cebu_places.name
         content = cebu_places.information
         picture1 = ""
         picture2 = ""
         picture3 = ""
+        picture4 = ""
+        picture5 = ""
+        picture6 = ""
     
-    elif spot == "":
+    elif spot == "Smooth Cafe, Moalboal":
         cebu_places=CebuPlaces.query.filter_by(name=spot).first()
+        TspotsName = cebu_places.name
         content = cebu_places.information
         picture1 = ""
         picture2 = ""
         picture3 = ""
+        picture4 = ""
+        picture5 = ""
+        picture6 = ""
     
-    elif spot == "":
+    elif spot == "The Pig & Palm":
         cebu_places=CebuPlaces.query.filter_by(name=spot).first()
+        TspotsName = cebu_places.name
         content = cebu_places.information
         picture1 = ""
         picture2 = ""
         picture3 = ""
+        picture4 = ""
+        picture5 = ""
+        picture6 = ""
     
-    elif spot == "":
+    elif spot == "Thai-Phoon, Bantayan Island":
         cebu_places=CebuPlaces.query.filter_by(name=spot).first()
+        TspotsName = cebu_places.name
         content = cebu_places.information
         picture1 = ""
         picture2 = ""
         picture3 = ""
+        picture4 = ""
+        picture5 = ""
+        picture6 = ""
+
+    elif spot == "Entoy's Bakasihan, Cordova":
+        cebu_places=CebuPlaces.query.filter_by(name=spot).first()
+        TspotsName = cebu_places.name
+        content = cebu_places.information
+        picture1 = ""
+        picture2 = ""
+        picture3 = ""
+        picture4 = ""
+        picture5 = ""
+        picture6 = ""
         
-    return render_template("mc-info.html", email=MyAcc, places=spot, cont=content, picture1=picture1, picture2=picture2, picture3=picture3)
+    return render_template("mc-info.html", email=MyAcc, places=spot, cont=content, picture1=picture1, picture2=picture2, picture3=picture3, picture4=picture4, picture5=picture5, picture6=picture6, TsName=TspotsName)
--- App/home/route.py
+++ App/home/route.py
@@ -9,4 +9,9 @@ def HomePage():
 
     return render_template("index.html")
 
+@home.route("/home/about")
+def AboutPage():
+
+    return render_template("about.html")
+
 
