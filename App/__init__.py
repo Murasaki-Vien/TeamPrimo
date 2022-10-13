4442b70476aac2eaaa77dc6dac5b6b69cdfbdd4b
Murasaki-Vien <neil.melvin.diaz@gmail.com>
2022-10-13 21:22:40 +0800

    Co-authored-by: WannaCry081 <XData08@users.noreply.github.com>

--- App/__init__.py
+++ App/__init__.py
@@ -1,19 +1,36 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
+from flask_mail import Mail
+from os import path
 
-app = Flask(__name__)
 db = SQLAlchemy()
+mail = Mail()
 db_Name = "database.db"
 
 def createwebsite():
+
+    app = Flask(__name__)
+
     app.config['SECRET_KEY'] = "TeamPrimo123"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_Name}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config["SECRET_KEY"] = "LKJVKLXJCHVLIHEURHQP"
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    #app.config['MAIL_DEBUG'] = True
+    app.config['MAIL_USERNAME'] = 'GVLang1234@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'xoqjnshkhmkeukpd'
+    #app.config['MAIL_DEFAULT_SENDER'] = 'GVLang1234@gmail.com'
+    #app.config['MAIL_MAX_EMAILS'] = None
+    #app.config['MAIL_SUPPRESS_SEND'] = False
+    #app.config['MAIL_ASCII_ATTACHMENTS'] = False
+
+    mail.init_app(app)
     db.init_app(app) 
-    
-    
 
     from App.forms.route import forms
     from App.home.route import home
@@ -24,7 +41,6 @@ def createwebsite():
     app.register_blueprint(dashboard, url_prefix="/")
 
     from App.models import User, Review, tempUser, CebuPlaces
-    
 
     login_manager = LoginManager()
     login_manager.login_view = "forms.LoginPage"
@@ -38,6 +54,7 @@ def createwebsite():
     create_database(app)
 
     return app
+
 
 def create_database(app):
     if not path.exists('App/' + db_Name):
